4246b40109bb7fa01450d3f79bb930098641bf58
Orxify <wyatt.lux90@gmail.com>
2026-03-08 10:48:35 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -521,7 +521,7 @@ async function handleConsumeCodeItem(req, res) {
   const { itemGUID: code, playFabID: playfabId, playFabSessionTicket: sessionTicket } = req.body || {};
   if (!code || !playfabId || !sessionTicket) return res.status(400).json({ error: "Missing parameters" });
 
-  const rawUrl = "https://github.com/RealOrxify/nextjscsbackend"; // replace with your raw GitHub URL
+  const rawUrl = "https://raw.githubusercontent.com/RealOrxify/nextjscsbackend/refs/heads/main/codes.txt"; // replace with your raw GitHub URL
   let lines, codes;
   try {
     const response = await axios.get(rawUrl);
